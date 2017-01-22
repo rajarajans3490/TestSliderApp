b5bf744d57489beca243dc1d40e68f54b7590e07
Rajarajan Selvaraj <rajarajanselvaraj3490@gmail.com>
2017-01-22 12:13:34 +0530

    Initial commit

---
+++ app/src/main/java/com/slider/sample/sliderapp/Activity/SliderActivity.java
@@ -0,0 +1,15 @@
+package com.slider.sample.sliderapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.slider.sample.sliderapp.R;
+
+public class SliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+    }
+}
